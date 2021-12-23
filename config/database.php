60af969450eab01849463c9a74a391942a2cf168
Abel Julca Landacay <91855596+AbelJulca@users.noreply.github.com>
2021-12-23 10:42:48 -0500

    cambio

--- config/database.php
+++ config/database.php
@@ -5,12 +5,3 @@ define('DB_PUERTO', '3306');
 define('DB_NAME', 'FACTUR');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
-/*define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_PUERTO', '3306');
-define('DB_NAME', 'uhaitwht_demo');
-define('DB_USER', 'uhaitwht_system');
-define('DB_PASS', 'Codiland2020');*/
-
-
